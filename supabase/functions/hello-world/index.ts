c9dcc6580b6ce0fa3fab2f4c46fd8cb4c4e8eec5
anil.mt <anil.nakum@manektech.com>
2023-05-16 17:08:41 +0530

    Test

---
+++ supabase/functions/hello-world/index.ts
@@ -0,0 +1,19 @@
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+
